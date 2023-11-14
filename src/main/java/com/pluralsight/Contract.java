642757e44d9d6075b6666535dcce2098ff8c8b93
JGraham-appdev <jgraham@appdev.yearup.org>
2023-11-14 01:56:35 -0500

    oneFiveSix

---
+++ src/main/java/com/pluralsight/Contract.java
@@ -0,0 +1,56 @@
+package com.pluralsight;
+
+public abstract class Contract {
+    private String dateOfContract;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+    private double totalPrice;
+    private double monthlyPayment;
+
+    public Contract(String dateOfContract, String customerName, String customerEmail, Vehicle vehicleSold, double totalPrice, double monthlyPayment) {
+        this.dateOfContract = dateOfContract;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+        this.totalPrice = 0;
+        this.monthlyPayment = 0;
+    }
+
+    public String getDateOfContract() {
+        return dateOfContract;
+    }
+
+    public void setDateOfContract(String dateOfContract) {
+        this.dateOfContract = dateOfContract;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    public abstract double getTotalPrice();
+
+    public abstract double getMonthlyPayment();
+
+}
---
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -0,0 +1,78 @@
+package com.pluralsight;
+
+import com.pluralsight.Contract;
+import com.pluralsight.Vehicle;
+
+public class SalesContract extends Contract {
+    private double salesTaxAmount;  //5%
+    private double recordingFee;    //$100
+    private double processingFee;   //$295 for vehicles under $10,000 and $495 for all others
+    private boolean financeOption;        //yes/no
+
+    public SalesContract(String dateOfContract, String customerName, String customerEmail, Vehicle vehicleSold, boolean financeOption) {
+        super(dateOfContract, customerName, customerEmail, vehicleSold);
+        this.financeOption = financeOption;
+
+    }
+
+    public double getSalesTaxAmount() {
+        return salesTaxAmount;
+    }
+
+    public void setSalesTaxAmount(double salesTaxAmount) {
+        this.salesTaxAmount = salesTaxAmount;
+    }
+
+    public double getRecordingFee() {
+        return recordingFee;
+    }
+
+    public void setRecordingFee(double recordingFee) {
+        this.recordingFee = recordingFee;
+    }
+
+    public double getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(double processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public boolean isFinanceOption() {
+        return financeOption;
+    }
+
+    public void setFinanceOption(boolean financeOption) {
+        this.financeOption = financeOption;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return getVehicleSold().getPrice() + salesTaxAmount + recordingFee + processingFee;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        int numberOfPayments = 0;
+        double interestRate = 0;
+        if (financeOption) {
+            if (getVehicleSold().getPrice() >= 10000) {
+                numberOfPayments = 48;
+                interestRate = 4.25 / 1200;
+            } else {
+                numberOfPayments = 24;
+                interestRate = 5.25 / 1200;
+            }
+
+            double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+            monthlyPayment = Math.round(monthlyPayment * 100);
+            monthlyPayment /= 100;
+            return monthlyPayment;
+        } else {
+            return 0.0;
+        }
+    }
+
+
+}
